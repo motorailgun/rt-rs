8ef51683bcf702e06c26c0b27cb0979c304927b1
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-06-17 09:52:54 +0900

    implement very early vec3d

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+pub mod vec3d;
+use vec3d::Vec3d;
+
 fn main() {
     println!("Hello, world!");
+    let x = Vec3d::<f64>::new();
+    println!("{}", x.dot(&x).x);
 }
---
+++ src/vec3d.rs
@@ -0,0 +1,23 @@
+use num_traits::Float;
+#[derive(Copy, Clone)]
+pub struct Vec3d<T: Float> {
+        pub x: T,
+        pub y: T,
+        pub z: T,
+}
+
+impl<T: Float> Vec3d<T> {
+    pub fn new() -> Self {
+        return Vec3d { x: T::zero(), y: T::zero(), z: T::zero() };
+    }
+
+    pub fn dot(&self, other: &Vec3d<T>) -> Vec3d<T> {
+        return Vec3d { x: self.x * other.x,
+                        y: self.y * other.y,
+                        z: self.z * other.z }
+    }
+
+    pub fn norm(&self) -> T {
+        return (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+    }
+}
