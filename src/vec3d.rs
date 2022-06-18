3d6f350e6502069b21ba04b887aef5300026e362
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-06-19 07:40:52 +0900

    impl Add and Sub for vec3d

--- src/vec3d.rs
+++ src/vec3d.rs
@@ -1,4 +1,6 @@
 use num_traits::Float;
+use std::ops::{Add, Sub, Mul, Div};
+
 #[derive(Copy, Clone)]
 pub struct Vec3d<T: Float> {
         pub x: T,
@@ -19,5 +21,27 @@ impl<T: Float> Vec3d<T> {
 
     pub fn norm(&self) -> T {
         return (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+    }
+}
+
+impl<T: Float> Add for Vec3d<T> {
+    type Output = Vec3d<T>;
+    fn add(self, other: Vec3d<T>) -> Self::Output {
+        return Vec3d {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z
+        };
+    }
+}
+
+impl<T: Float> Sub for Vec3d<T> {
+    type Output = Vec3d<T>;
+    fn sub(self, other: Vec3d<T>) -> Self::Output {
+        return Vec3d {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z
+        };
     }
 }
