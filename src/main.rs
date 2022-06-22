f47f7beac7df976d6ee4ff62f36bb25d05f6a66a
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-06-22 15:30:50 +0900

    main: chotto shinchoku

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 pub mod vec3d;
+pub mod color;
+
 use vec3d::Vec3d;
+use color::Color;
+
+fn ray_color(ray: Vec3d<f64>) -> Color<u8> {
+    
+}
 
 fn main() {
-    println!("Hello, world!");
-    let x = Vec3d::<f64>::new();
-    println!("{}", x.dot(&x).x);
+    const ASPECT_RATIO: usize = (16. / 9.) as usize;
+    const IMAGE_HEIGHT: usize = 200;
+    const IMAGE_WIDTH: usize = IMAGE_HEIGHT * ASPECT_RATIO;
+
+    let mut canvas: [[i32; IMAGE_HEIGHT]; IMAGE_WIDTH];
+
+
 }
