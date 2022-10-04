185be5f750b053dd93d06b209b72eabd291faaca
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-10-04 15:36:46 +0900

    Color: impl Display

--- src/color.rs
+++ src/color.rs
@@ -1,8 +1,15 @@
 use num_traits::PrimInt;
+use std::fmt;
 
 #[derive(Clone, Copy)]
-pub struct Color<T: PrimInt> {
+pub struct Color<T: PrimInt + fmt::Display> {
     pub r: T,
     pub g: T,
     pub b: T,
+}
+
+impl<T: PrimInt + fmt::Display> fmt::Display for Color<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.r, self.g, self.b)
+    }
 }
