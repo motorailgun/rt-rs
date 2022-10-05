26467718b0d38e08f3318cd86e5e0cc0f4024ab5
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-10-06 02:59:08 +0900

    Color: add to_int

--- src/color.rs
+++ src/color.rs
@@ -1,15 +1,69 @@
-use num_traits::PrimInt;
-use std::fmt;
+use num_traits::{Num, Unsigned, PrimInt, NumCast};
+use std::ops::{Add, Mul, Rem};
+use std::fmt::{self, Display};
 
 #[derive(Clone, Copy)]
-pub struct Color<T: PrimInt + fmt::Display> {
+pub struct Color<T: Num + fmt::Display> {
     pub r: T,
     pub g: T,
     pub b: T,
 }
 
-impl<T: PrimInt + fmt::Display> fmt::Display for Color<T> {
+impl<T: Num + fmt::Display> fmt::Display for Color<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} {} {}", self.r, self.g, self.b)
+    }
+}
+
+impl<T: Num + Copy + fmt::Display> Add for Color<T> {
+    type Output = Color<T>;
+    fn add(self, other: Self) -> Self::Output {
+        Color::<T> {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+        }
+    }
+}
+
+impl<T: Num + Copy + fmt::Display> Mul<T> for Color<T> {
+    type Output = Color<T>;
+    fn mul(self, other: T) -> Self::Output {
+        Color::<T> {
+            r: self.r * other,
+            g: self.g * other,
+            b: self.b * other,
+        }
+    }
+}
+
+impl<T: Num + Copy + fmt::Display> Rem<T> for Color<T> {
+    type Output = Color<T>;
+    fn rem(self, other: T) -> Self::Output {
+        Color::<T> {
+            r: self.r % other,
+            g: self.g % other,
+            b: self.b % other,
+        }
+    }
+}
+
+impl<T: Num + fmt::Display + NumCast + Copy> Color<T> {
+    fn clamp(&self) -> Color<T> {
+        *self % T::one()
+    }
+
+    pub fn to_int<G: PrimInt + Unsigned + Display>(&self) -> Option<Color<G>>{
+        let clamped = self.clamp();
+
+        if let Some(max_value) = T::from(G::max_value()) {
+            let Color{r, g, b} = clamped * max_value;
+            G::from(r)
+                .zip(G::from(g))
+                .zip(G::from(b))
+                .map(|((r, g), b)| Color{r, g, b})
+        } else {
+            None
+        }
     }
 }
