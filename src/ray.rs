c421635d725800de43accd40dff1237d1363a6f6
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-10-06 02:58:38 +0900

    Ray: pub

--- src/ray.rs
+++ src/ray.rs
@@ -2,6 +2,6 @@ use crate::vec3d::Vec3d;
 use num_traits::Float;
 
 pub struct Ray<T: Float> {
-    start: Vec3d<T>,
-    direction: Vec3d<T>,
+    pub start: Vec3d<T>,
+    pub direction: Vec3d<T>,
 }
