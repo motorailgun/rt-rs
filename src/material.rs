3cf499d93c50a68ad556cbe64b025a44b4e8c599
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-11-02 10:16:07 +0900

    changing many, will implement det and intersect later

--- src/material.rs
+++ src/material.rs
@@ -1,22 +1,11 @@
 use std::fmt::Display;
-use crate::{ray::Ray, color::Color, vec3d::Vec3d, utils::*};
+use crate::color::Color;
 use num_traits::Float;
-use rand::{self, prelude::Distribution, distributions::Standard};
 
-pub trait Material<T: Float> {
-    fn reflect(&self, hit_point: Vec3d<T>) -> Ray<T>;
-}
+pub trait Material {}
 
 pub struct Lambertian<T: Float + Display> {
     pub color: Color<T>
 }
 
-impl<T> Material<T> for Lambertian<T> 
-        where T: Float + Display, Standard: Distribution<T> {
-    fn reflect(&self, hit_point: Vec3d<T>) -> Ray<T> {
-        Ray { 
-            start: hit_point,
-            direction: random_vec_in_unit_sphere::<T>(),
-        }
-    }
-}
+impl<T: Float + Display> Material for Lambertian<T>{}
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,12 +1,24 @@
 use num_traits::Float;
-use crate::{ray::Ray, vec3d::Vec3d};
+use crate::{ray::Ray, vec3d::Vec3d, material::Material};
 
 pub trait Primitive {
     fn intersect<T: Float>(&self, incoming_ray: &Ray<T>) -> Option<Ray<T>>;
 }
 
-pub struct Sphere<T: Float, G: Primitive> {
+pub struct Sphere<T: Float, G: Material> {
     pub center: Vec3d<T>,
     pub radius: T,
     pub material: G,
+}
+
+impl<T: Float, G: Material> Sphere<T, G> {
+    fn determine(&self) -> Option<Vec3d<T>>{
+        
+    }
+}
+
+impl<T: Float, G: Material> Primitive for Sphere<T, G> {
+    fn intersect(&self, incoming_ray: &Ray<T>) -> Option<Ray<T>> {
+
+    }
 }
