da4c79e9d04f1dfb0210fc004e50307603978422
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-11-14 10:58:45 +0900

    materia;/primitive: fix moromoro

--- src/material.rs
+++ src/material.rs
@@ -1,11 +1,21 @@
 use std::fmt::Display;
-use crate::color::Color;
+use crate::{color::Color, vec3d::Vec3d, utils::*};
 use num_traits::Float;
+use rand::{self, prelude::Distribution, distributions::Standard};
 
-pub trait Material {}
+pub trait Material<T: Float> {
+    fn reflect(&self, hit_point: &Vec3d<T>) -> Vec3d<T>;
+}
 
 pub struct Lambertian<T: Float + Display> {
     pub color: Color<T>
 }
 
-impl<T: Float + Display> Material for Lambertian<T>{}
+impl<T> Material<T> for Lambertian<T> 
+where T: Float + Display, Standard: Distribution<T> {
+    #[allow(unused_variables)]
+    fn reflect(&self, hit_point: &Vec3d<T>) -> Vec3d<T> {
+        random_vec_in_unit_sphere()
+        // todo: must fix this to emit outgoing vector using normal
+    }
+}
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,24 +1,31 @@
 use num_traits::Float;
 use crate::{ray::Ray, vec3d::Vec3d, material::Material};
 
-pub trait Primitive {
-    fn intersect<T: Float>(&self, incoming_ray: &Ray<T>) -> Option<Ray<T>>;
+pub trait Primitive<T: Float> {
+    fn intersect(&self, incoming_ray: &Ray<T>) -> Option<Ray<T>>;
 }
 
-pub struct Sphere<T: Float, G: Material> {
+pub struct Sphere<T: Float, G: Material<T>> {
     pub center: Vec3d<T>,
     pub radius: T,
     pub material: G,
 }
 
-impl<T: Float, G: Material> Sphere<T, G> {
-    fn determine(&self) -> Option<Vec3d<T>>{
-        
-    }
-}
-
-impl<T: Float, G: Material> Primitive for Sphere<T, G> {
+impl<T: Float, G: Material<T>> Primitive<T> for Sphere<T, G> {
     fn intersect(&self, incoming_ray: &Ray<T>) -> Option<Ray<T>> {
+        let center_to_origin = incoming_ray.start - self.center;
+        let a = incoming_ray.direction.norm();
+        let half_b = center_to_origin.dot(&incoming_ray.direction);
+        let c = center_to_origin.norm() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
 
+        if discriminant < T::from(0.0)? {
+            None
+        } else {
+            Some(Ray::<T> {
+                start: incoming_ray.start + incoming_ray.direction * discriminant,
+                direction: self.material.reflect(&(incoming_ray.start + incoming_ray.direction * discriminant))
+            })
+        }
     }
 }
