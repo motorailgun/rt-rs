26dff095c7f2ec9156b8baf4a3c1bc8907164a43
motorailgun <motoi_public@mail.aria-on-the-planet.es>
2022-11-22 00:03:23 +0900

    main.rs: minor changes, move print to closure

--- src/main.rs
+++ src/main.rs
@@ -56,27 +56,30 @@ fn ray_color<T: FloatU + Display>(ray: Ray<T>, world: &Vec<Box<dyn Primitive<T>>
 
 fn main() {
     const ASPECT_RATIO: f32 = 16. / 9.;
-    const IMAGE_HEIGHT: usize = 200;
-    const IMAGE_WIDTH: usize = ((IMAGE_HEIGHT as f32) * ASPECT_RATIO) as usize;
+    const IMAGE_WIDTH: usize = 400;
+    const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as usize;
     const RAY_COUNT: usize = 100;
     const SAMPLE_COUNT: usize = 100;
 
     let origin = Vec3d{x: 0., y: 0., z: 0.};
 
-    // TODO: These values must be bigger I guess?
-    let (lu_x, ru_x) = (-1.0, 1.0);
-    let (lu_y, rd_y) = (ru_x / ASPECT_RATIO, -ru_x / ASPECT_RATIO);
-    let z_center = 1.0;
+    let viewport_height: f32 = 2.0;
+    let viewport_width = viewport_height * ASPECT_RATIO;
+    let z_center = -1.0;
+
+    let horizontal = Vec3d{x: viewport_width, y: 0., z: 0.};
+    let vertical = Vec3d{x: 0., y: viewport_height, z: 0.};
+    let upper_left_corner = origin - horizontal / 2. + vertical / 2. + Vec3d{x: 0., y: 0., z: z_center};
 
     let world: Vec<Box<dyn Primitive<f32>>> = vec![
         Box::new(Sphere{
-            center: Vec3d{x: 0., y: -0.5, z: 1.},
+            center: Vec3d{x: 0., y: 0., z: -1.},
             radius: 0.5,
             material: sphere_material::Lambertian{},
             color: Color{r: 0.8, g: 0.0, b: 0.0},
         }),
         Box::new(Sphere{
-            center: Vec3d{x: 0., y: -101., z: 1.},
+            center: Vec3d{x: 0., y: -100.5, z: -1.},
             radius: 100.,
             material: sphere_material::Lambertian{},
             color: Color{r: 0.0, g: 0.8, b: 0.0},
@@ -85,17 +88,12 @@ fn main() {
 
     let canvas: Vec<Vec<Color<f32>>> = (0..IMAGE_HEIGHT).into_iter().map(|y: usize| {
         (0..IMAGE_WIDTH).into_iter().map(|x: usize| {
-            let x_factor = x as f32 / IMAGE_WIDTH as f32;
-            let y_factor = y as f32 / IMAGE_HEIGHT as f32;
+            let x_factor = x as f32 / (IMAGE_WIDTH - 1) as f32;
+            let y_factor = y as f32 / (IMAGE_HEIGHT - 1) as f32;
 
             let ray = Ray{
                 start: origin,
-                direction: Vec3d {
-                    // TODO: check this formula
-                    x: lu_x + (ru_x - lu_x) * x_factor,
-                    y: lu_y + (rd_y - lu_y) * y_factor,
-                    z: z_center,
-                }.unitize()
+                direction: upper_left_corner + horizontal * x_factor - vertical * y_factor,
             };
             ray_color(ray, &world, RAY_COUNT)
         }).collect()
@@ -104,16 +102,20 @@ fn main() {
 
     println!("P3\n{} {}\n{}", IMAGE_WIDTH, IMAGE_HEIGHT, 255);
 
-    canvas.iter().for_each(|line| {
-        let mut pr_string: String = String::from("");
-        line.iter().for_each(|pixel| {
-            let pixel_int = match pixel.to_int::<u8>() {
-                Some(px) => px,
-                None => panic!("Error converting pixel to integer format")
-            };
+    let print_result = || {
+            canvas.iter().for_each(|line| {
+            let mut pr_string: String = String::from("");
+            line.iter().for_each(|pixel| {
+                let pixel_int = match pixel.to_int::<u8>() {
+                    Some(px) => px,
+                    None => panic!("Error converting pixel to integer format")
+                };
 
-            pr_string = format!("{} {}", pr_string, pixel_int);
-        });
-        println!("{}", pr_string);
-    })
+                pr_string = format!("{} {}", pr_string, pixel_int);
+            });
+            println!("{}", pr_string);
+        })
+    };
+
+    print_result();
 }
