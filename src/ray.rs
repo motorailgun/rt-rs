b85a38115e702fbe31e7e568c00f401bdb7a2838
motorailgun <motoi_public@mail.aria-on-the-planet.es>
2022-11-22 21:21:42 +0900

    ray: add Copy and Clone

--- src/ray.rs
+++ src/ray.rs
@@ -1,7 +1,7 @@
 use crate::vec3d::Vec3d;
 use num_traits::Float;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Ray<T: Float> {
     pub start: Vec3d<T>,
     pub direction: Vec3d<T>,
