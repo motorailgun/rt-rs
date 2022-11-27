aa9f128f5d9fabbfaf43b30e75b0b17fc3254d3d
motorailgun <motoi_public@mail.aria-on-the-planet.es>
2022-11-28 04:21:11 +0900

    material: front face's normal

--- src/material.rs
+++ src/material.rs
@@ -12,6 +12,17 @@ pub mod sphere_material {
     use crate::{vec3d::Vec3d, utils::*, primitives::Sphere, ray::Ray};
     use num_traits::Float;
     use rand::{self, prelude::Distribution, distributions::Standard};
+
+    #[inline]
+    fn front_faced_normal<T: Float + Display, U: super::Material<T>>(prim: &Sphere<T, U>, hit_point: &Vec3d<T>, incoming_ray: &Ray<T>) -> Vec3d<T> {
+        let normal = *hit_point - prim.center;
+        if incoming_ray.direction.dot(&normal) > T::from(0.0).unwrap() {
+            -normal
+        } else {
+            normal
+        }
+    }
+
     pub struct Lambertian {
     }
 
@@ -19,7 +30,8 @@ pub mod sphere_material {
     where T: Float + Display, Standard: Distribution<T> {
         #[allow(unused_variables)]
         fn reflect<U: super::Material<T>>(&self, prim: &Sphere<T, U>, hit_point: &Vec3d<T>, incoming_ray: &Ray<T>) -> Vec3d<T> {
-            random_vec_in_unit_sphere()+ (*hit_point - prim.center).unitize() * T::from(1.00001).unwrap() 
+            let normal = front_faced_normal(prim, hit_point, incoming_ray);
+            random_vec_in_unit_sphere() + normal.unitize() * T::from(1.00001).unwrap() 
             // todo: fix near-zero vectors
         }
     }
@@ -29,7 +41,7 @@ pub mod sphere_material {
     impl<T> super::Material<T> for Metal
     where T: Float + Display {
         fn reflect<U: super::Material<T>>(&self, prim: &Sphere<T, U>, hit_point: &Vec3d<T>, incoming_ray: &Ray<T>) -> Vec3d<T> {
-            let norm_unit = (*hit_point - prim.center).unitize();
+            let norm_unit = front_faced_normal(prim, hit_point, incoming_ray).unitize();
             incoming_ray.direction - norm_unit * incoming_ray.direction.dot(&norm_unit) * T::from(2.0).unwrap()
         }
     }
