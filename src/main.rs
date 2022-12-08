97da4c3d96783d2c8963dcdd616d19e75ffbd879
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-12-08 13:50:22 +0900

    main: add render() and fix out_image

--- src/main.rs
+++ src/main.rs
@@ -39,18 +39,32 @@ fn out_image<T: Float, U: PrimInt + Display>(canvas: &Vec<Vec<Color<T>>>, width:
     for line in canvas.iter() {
         for pixel in line.iter() {
             let pix_i = pixel.to_int::<U>();
-            println!("{} {} {} ", pix_i.r, pix_i.g, pix_i.b);
+            print!("{} {} {} ", pix_i.r, pix_i.g, pix_i.b);
         }
-        print!("");
+        println!("");
     }
 }
 
+fn render<T: Float>(width: usize, height: usize) -> Vec<Vec<Color<T>>> {
+    (0..height).collect::<Vec<usize>>().iter().enumerate().map(|(y, _)| {
+        (0..width).collect::<Vec<usize>>().iter().enumerate().map(|(x, _)| -> Color<T> {
+            let r = T::from(y).unwrap() / T::from(height).unwrap();
+            let g = T::from(x).unwrap() / T::from(width).unwrap();
+            let b = T::from(0.25).unwrap();
+
+            Color{r, g, b}
+        }).collect()
+    }).collect()
+}
+
 fn main() {
-    let aspect_ratio: f64 = 16. / 9.;
+    #[allow(non_camel_case_types)]
+    type float_t = f64;
+
+    let aspect_ratio: float_t = 16. / 9.;
     let image_width: usize = 400;
-    let image_height = (400 as f64 / aspect_ratio) as usize;
+    let image_height = (400 as float_t / aspect_ratio) as usize;
 
-    let canvas: Vec<Vec<Color<f64>>>;
-
-    
+    let canvas: Vec<Vec<Color<float_t>>> = render(image_width, image_height);
+    out_image::<float_t, u8>(&canvas, image_width, image_height);
 }
