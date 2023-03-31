0899051eb2521468f60ab730ea5a912d59ed15f2
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-04-01 06:48:56 +0900

    add pubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 pub mod color;
+pub mod ray;
 pub mod vec3;
+pub mod point3;
 
 pub mod prelude {
     pub type Vec3<T> = super::vec3::Vec3<T>;
     pub type Color<T> = super::color::Color<T>;
     pub type Point3<T> = Vec3<T>;
+    pub type Ray<T> = super::ray::Ray<T>;
 }
